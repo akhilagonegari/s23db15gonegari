b27ed72be5d29fe8c884e810ff8921f8aa8fc33d
Akhila Gonegari <142836078+akhilagonegari@users.noreply.github.com>
2023-11-15 19:06:51 -0600

    updated pug

--- controllers/ant.js
+++ controllers/ant.js
@@ -125,4 +125,46 @@ exports.ant_create_post = async function(req, res) {
     }
     };   
     
+    // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.ant_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('antcreate', { title: 'ant Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+};
+
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.ant_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await ant.findById(req.query.id)
+    res.render('antupdate', { title: 'ant Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle a delete one view with id from query
+exports.ant_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await ant.findById(req.query.id)
+    res.render('antdelete', { title: 'ant Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
     
--- routes/ant.js
+++ routes/ant.js
@@ -6,7 +6,18 @@ router.get('/', ant_controlers.ant_view_all_Page );
 // GET request for one costume.
 router.get('/ant/:id', ant_controlers.ant_detail);
 
-
 // GET detail ant page */
 router.get('/detail', ant_controlers.ant_view_one_Page);
+
+// GET create costume page */
+router.get('/create', ant_controlers.ant_create_Page);
+
+// GET create update page */
+router.get('/update', ant_controlers.ant_update_Page);
+
+// GET delete costume page */
+router.get('/delete', ant_controlers.ant_delete_Page);
+
+
+
 module.exports = router;
